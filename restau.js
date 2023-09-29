e93dfc577af0b6ef92ef528b2715fc44cc075165
Archimonde32 <dkgaming718@gmail.com>
2023-09-29 14:56:30 +0200

    nav

--- restau.js
+++ restau.js
@@ -7,10 +7,10 @@ document.addEventListener("DOMContentLoaded", () => {
         menuToggle.classList.toggle("active");
     });
 });
+
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
-
         const targetId = this.getAttribute('href').substring(1);
         const targetElement = document.getElementById(targetId);
 
@@ -22,3 +22,33 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         }
     });
 });
+
+
+
+const navLinks = document.querySelectorAll(".nav-links a");
+
+
+function updateActiveLinkColor() {
+    
+    navLinks.forEach((link) => {
+        const sectionId = link.getAttribute("href").substring(1); 
+        const section = document.getElementById(sectionId); 
+        const rect = section.getBoundingClientRect(); 
+        
+        if (
+            rect.top >= 0 &&
+            rect.bottom <= window.innerHeight
+        ) {
+            link.style.color = "#ff5722"; 
+        } else {
+            link.style.color = ""; 
+        }
+    });
+}
+
+
+window.addEventListener("scroll", updateActiveLinkColor);
+window.addEventListener("resize", updateActiveLinkColor);
+
+
+updateActiveLinkColor();
